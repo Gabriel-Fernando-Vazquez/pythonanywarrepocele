75178ed66aaa9f6768128117d92c783e72e75503
Gabriel Vazquez <kuroshimadara@gmail.com>
2023-07-30 11:47:44 -0300

    modificado para pythonanyware creo XD

--- ProyectoFinal/ProyectoFinal/settings/local.py
+++ ProyectoFinal/ProyectoFinal/settings/local.py
@@ -1,16 +1,11 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['Gabriel94.mysql.pythonanywhere-services.com']
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'new_db.sqlite3',
-    }
-}
+
---
+++ ProyectoFinal/ProyectoFinal/settings/production.py
@@ -0,0 +1,15 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Gabriel94$default',
+        'USER': 'Gabriel94',
+        'PASSWORD':'Gabilotemysql1994',
+        'HOST': 'Gabriel94.mysql.pythonanywhere-services.com'
+
+    }
+}
